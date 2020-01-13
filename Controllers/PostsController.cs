1266deced5b52020a5f6eefc1af534455edfaff6
Mitrofanov Daniil <nonexistent-man@yandex.ru>
2020-01-13 05:28:37 +0200

    Posts controller

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
+using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PikabuApi.Helpers;
 using PikabuApi.Models;
 using PikabuApi.Parsers;
 
@@ -26,17 +25,19 @@ namespace PikabuApi.Controllers
         public async Task<Post> GetSinglePost(int postId)
         {
             PostsParser parser = new PostsParser();
-            var result = new List<Post>();
-            using (var httpClient = new HttpClient())
+            Post result = null;
+            var htmlSource = await NetworkHelper.GetHtmlPageSource("https://pikabu.ru/story/_" + postId);
+
+            if(htmlSource != null)
             {
-                var request = await httpClient.GetAsync("https://www.pikabu.ru/story/_" + postId);
-                if (request.IsSuccessStatusCode)
+                var resultList = await parser.ParseAsync(htmlSource);
+                if(resultList != null && resultList.Count != 0)
                 {
-                    var str = await request.Content.ReadAsStringAsync();
-                    result = await parser.ParseAsync(str);
+                    result = resultList[0];
                 }
             }
-            return result[0];
+
+            return result;
         }
 
         // /api/posts/hot?p=1&c=2
@@ -46,17 +47,37 @@ namespace PikabuApi.Controllers
         public async Task<List<Post>> GetHotPosts()
         {
             PostsParser parser = new PostsParser();
-            var result = new List<Post>();
-            using (var httpClient = new HttpClient())
+            List<Post> result = new List<Post>();
+            int page = 1, count = 1;
+
+            if(Request.Query.ContainsKey("p") && Request.Query["p"].Count == 1)
             {
-                var request = await httpClient.GetAsync("https://www.pikabu.ru/hot?page=1");
-                if(request.IsSuccessStatusCode)
-                {
-                    var str = await request.Content.ReadAsStringAsync();
-                    result = await parser.ParseAsync(str);
-                }
-                
+                int.TryParse(Request.Query["p"][0], out page);
             }
+
+            if (Request.Query.ContainsKey("c") && Request.Query["c"].Count == 1)
+            {
+                int.TryParse(Request.Query["c"][0], out count);
+            }
+
+            count = count > 5 ? 5 : count;
+            count = count < 1 ? 1 : count;
+            page = page < 1 ? 1 : page;
+
+            for (int i = page; i < page + count; i++)
+            {
+                string htmlSource = await NetworkHelper.GetHtmlPageSource("https://pikabu.ru/hot?page=" + i);
+
+                if (htmlSource != null)
+                {
+                    var resultList = await parser.ParseAsync(htmlSource);
+                    if(resultList != null)
+                    {
+                        result.AddRange(resultList);
+                    }
+                }
+            }
+
             return result;
         }
 
@@ -66,36 +87,186 @@ namespace PikabuApi.Controllers
         // p - page number
         // c - page count
         [HttpGet("best")]
-        public List<Post> GetBestPosts()
+        public async Task<List<Post>> GetBestPosts()
         {
-            return null;
+            PostsParser parser = new PostsParser();
+            List<Post> result = new List<Post>();
+            int page = 1, count = 1;
+            DateTime date = new DateTime(1998, 8, 16);
+
+            if (Request.Query.ContainsKey("p") && Request.Query["p"].Count == 1)
+            {
+                int.TryParse(Request.Query["p"][0], out page);
+            }
+
+            if (Request.Query.ContainsKey("c") && Request.Query["c"].Count == 1)
+            {
+                int.TryParse(Request.Query["c"][0], out count);
+            }
+
+            if (Request.Query.ContainsKey("d") && Request.Query["d"].Count == 1)
+            {
+                int.TryParse(Request.Query["d"][0], out count);
+
+                try
+                {
+                    date = DateTime.ParseExact(Request.Query["d"][0], "dd-MM-yyyy", CultureInfo.InvariantCulture);
+                }
+                catch
+                {
+                    return result;
+                }
+            }
+
+            count = count > 5 ? 5 : count;
+            count = count < 1 ? 1 : count;
+            page = page < 1 ? 1 : page;
+
+            string url = "https://pikabu.ru/best?page=";
+            if(date == new DateTime(1998, 8, 16))
+            {
+                url = "https://pikabu.ru/best/" + date.ToString("dd-MM-yyyy") + "?page=";
+            }
+
+            for (int i = page; i < page + count; i++)
+            {
+
+                string htmlSource = await NetworkHelper.GetHtmlPageSource(url + i);
+
+                if (htmlSource != null)
+                {
+                    var resultList = await parser.ParseAsync(htmlSource);
+                    if (resultList != null)
+                    {
+                        result.AddRange(resultList);
+                    }
+                }
+            }
+
+            return result;
         }
 
         // /api/posts/new?p=1&c=2
         // p - page number
         // c - page count
         [HttpGet("new")]
-        public List<Post> GetNewPosts()
+        public async Task<List<Post>> GetNewPosts()
         {
-            return null;
+            PostsParser parser = new PostsParser();
+            List<Post> result = new List<Post>();
+            int page = 1, count = 1;
+
+            if (Request.Query.ContainsKey("p") && Request.Query["p"].Count == 1)
+            {
+                int.TryParse(Request.Query["p"][0], out page);
+            }
+
+            if (Request.Query.ContainsKey("c") && Request.Query["c"].Count == 1)
+            {
+                int.TryParse(Request.Query["c"][0], out count);
+            }
+
+            count = count > 5 ? 5 : count;
+            count = count < 1 ? 1 : count;
+            page = page < 1 ? 1 : page;
+
+            for (int i = page; i < page + count; i++)
+            {
+                string htmlSource = await NetworkHelper.GetHtmlPageSource("https://pikabu.ru/new?page=" + i);
+
+                if (htmlSource != null)
+                {
+                    var resultList = await parser.ParseAsync(htmlSource);
+                    if (resultList != null)
+                    {
+                        result.AddRange(resultList);
+                    }
+                }
+            }
+
+            return result;
         }
 
         // /api/posts/user/userName?p=1&c=2
         // p - page number
         // c - page count
         [HttpGet("user/{userName}")]
-        public List<Post> GetUserPosts(string userName)
+        public async Task<List<Post>> GetUserPosts(string userName)
         {
-            return null;
+            PostsParser parser = new PostsParser();
+            List<Post> result = new List<Post>();
+            int page = 1, count = 1;
+
+            if (Request.Query.ContainsKey("p") && Request.Query["p"].Count == 1)
+            {
+                int.TryParse(Request.Query["p"][0], out page);
+            }
+
+            if (Request.Query.ContainsKey("c") && Request.Query["c"].Count == 1)
+            {
+                int.TryParse(Request.Query["c"][0], out count);
+            }
+
+            count = count > 5 ? 5 : count;
+            count = count < 1 ? 1 : count;
+            page = page < 1 ? 1 : page;
+
+            for (int i = page; i < page + count; i++)
+            {
+                string htmlSource = await NetworkHelper.GetHtmlPageSource("https://pikabu.ru/@" + userName + "?page=" + i);
+
+                if (htmlSource != null)
+                {
+                    var resultList = await parser.ParseAsync(htmlSource);
+                    if (resultList != null)
+                    {
+                        result.AddRange(resultList);
+                    }
+                }
+            }
+
+            return result;
         }
 
         // /api/posts/community/communityName?p=1&c=2
         // p - page number
         // c - page count
         [HttpGet("community/{communityName}")]
-        public List<Post> GetCommunityPosts(string userName)
+        public async Task<List<Post>> GetCommunityPosts(string communityName)
         {
-            return null;
+            PostsParser parser = new PostsParser();
+            List<Post> result = new List<Post>();
+            int page = 1, count = 1;
+
+            if (Request.Query.ContainsKey("p") && Request.Query["p"].Count == 1)
+            {
+                int.TryParse(Request.Query["p"][0], out page);
+            }
+
+            if (Request.Query.ContainsKey("c") && Request.Query["c"].Count == 1)
+            {
+                int.TryParse(Request.Query["c"][0], out count);
+            }
+
+            count = count > 5 ? 5 : count;
+            count = count < 1 ? 1 : count;
+            page = page < 1 ? 1 : page;
+
+            for (int i = page; i < page + count; i++)
+            {
+                string htmlSource = await NetworkHelper.GetHtmlPageSource("https://pikabu.ru/community/" + communityName +"?page=" + i);
+
+                if (htmlSource != null)
+                {
+                    var resultList = await parser.ParseAsync(htmlSource);
+                    if (resultList != null)
+                    {
+                        result.AddRange(resultList);
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
---
+++ Helpers/NetworkHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PikabuApi.Helpers
+{
+    public static class NetworkHelper
+    {
+        private static readonly HttpClient _httpClient;
+
+        static NetworkHelper()
+        {
+            var handler = new HttpClientHandler();
+            handler.MaxConnectionsPerServer = int.MaxValue;
+            _httpClient = new HttpClient(handler);
+        }
+
+        public static async Task<string> GetHtmlPageSource(string url = "")
+        {
+            var response = await _httpClient.GetAsync(url);
+            string result = null;
+            if(response != null && response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+            return result;
+        }
+    }
+}
